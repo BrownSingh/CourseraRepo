712457741f22e9179f522b5c7fb52258b2f27590
BrownSingh <144540978+BrownSingh@users.noreply.github.com>
2023-09-11 13:08:26 -0400

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,9 @@
+greeting = 'Hello, friend!'
+print(greeting)
+greeting = 'Hi, everybody!'
+print(greeting)
+
+# variables can only start with string or underscore sign
+# variables cannot start with numeric values however it can end with numers
+# lower case and upper case variables are treated differently
+# Python has keywords that cannot be used as variable which are from and global
